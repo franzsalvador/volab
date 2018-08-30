695dad84f2330eec3d33e82a04dd3c5da6f1aa66
FranzCSUF <fsalvador79@gmail.com>
2018-08-30 11:04:06 -0700

    Create a React class component for the navigation bar.#1

---
+++ client/app.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import NavBar from './components/nav-bar'
+
+export default class App extends Component {
+
+  render() {
+    return (
+      <div className="container">
+        <NavBar/>
+
+      </div>
+    )
+  }
+}
