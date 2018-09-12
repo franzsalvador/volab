009b97c26d32a87dce953a91537e79cfaf057f8b
FranzCSUF <fsalvador79@gmail.com>
2018-09-11 17:32:00 -0700

    Remove handleClickVocalist onClick event handler and change the handleClickProducer event handler to handleCreateProfile. #12

--- client/components/profile-selectors.js
+++ client/components/profile-selectors.js
@@ -3,26 +3,19 @@ import React from 'react'
 export default class ProfileSelectors extends React.Component {
   constructor(props) {
     super(props)
-    this.handleClickProducer = this.handleClickProducer.bind(this)
-    this.handleClickVocalist = this.handleClickVocalist.bind(this)
+    this.handleCreateProfile = this.handleCreateProfile.bind(this)
   }
 
-  handleClickProducer() {
+  handleCreateProfile() {
     const { navigate } = this.props
-    navigate({ path: 'create-producer-profile' })
-  }
-
-  handleClickVocalist() {
-    const { navigate } = this.props
-    navigate({ path: 'create-vocalist-profile' })
+    navigate({ path: 'create--profile' })
   }
 
   render() {
-    const { handleClickProducer, handleClickVocalist } = this
+    const { handleCreateProfile } = this
     return (
       <div className="mb-5" id="profile-selectors">
-        <button className="mr-2 btn btn-outline-light" onClick={ handleClickProducer } type="button">Producer</button>
-        <button className="ml-2 btn btn-outline-light" onClick={ handleClickVocalist }type="button">Vocalist</button>
+        <button className="mr-2 btn btn-outline-light" onClick={ handleCreateProfile } type="button">Create Profile</button>
       </div>
     )
   }
