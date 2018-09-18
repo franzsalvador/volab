44ef90aec21e43766e8b598621e483b0f6eeb925
FranzCSUF <fsalvador79@gmail.com>
2018-09-18 09:21:46 -0700

    Create a route in that will handle GET requests for the url artists/followers/displayName that will return the list of followers for the artist the user is enquiring about. #17

--- client/app.js
+++ client/app.js
@@ -80,7 +80,7 @@ export default class App extends Component {
         return (
           <ViewConnections
             navigate = {navigate}
-            user = {user}
+            path = {path}
             params = {params}
             updateUser = {updateUser}/>
         )
--- client/containers/view-connections.js
+++ client/containers/view-connections.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react'
-import { Jumbotron, Container, Button } from 'reactstrap'
 
 export default class ViewConnections extends Component {
   constructor(props) {
@@ -11,15 +10,14 @@ export default class ViewConnections extends Component {
     this.handleViewProfile = this.handleViewProfile.bind(this)
   }
   componentDidMount() {
-    window.addEventListener('hashchange', () => location.reload())
-    const { artist } = this.props.params
-    fetch('/artists/followers/' + artist)
+    const { displayName } = this.props.params
+    fetch('/artists/followers/' + displayName)
       .then(res => res.ok ? res.json() : null)
-      .then(followers => this.setState({ followers }))
+      .then(followers => this.setState({ followers: followers }))
       .catch(err => console.error(err))
-    fetch('/artists/followering/' + artist)
+    fetch('/artists/following/' + displayName)
       .then(res => res.ok ? res.json() : null)
-      .then(followers => this.setState({ followers }))
+      .then(following => this.setState({ following: following }))
       .catch(err => console.error(err))
   }
   handleViewProfile(event) {
@@ -27,30 +25,10 @@ export default class ViewConnections extends Component {
     const displayName = event.target.name
     navigate({ path: 'view-profile', params: { 'displayName': displayName } })
   }
+
   render() {
-    console.log(this.state)
-    const { path } = this
-    const { followers } = this.state
-    const { handleViewProfile } = this
     return (
       <div>
-        <Container className="container bg-white mt-5">
-          <Jumbotron className="bg-white pt-4 mb-0">
-            <h5 className="mb-5 font-weight-bold">{ path === 'view-followers' ? 'Followers' : 'Following' }</h5>
-            {followers.map((artist, index) => {
-              return (
-                <div key= { index }>
-                  <div className="profile-image-explore-view float-md-left rounded-circle" style={{ backgroundImage: `url(${artist.imageUrl})` }}></div>
-                  <p className="profile-display-name-explore-view mb-0">{artist.displayName + ' (' + artist.artistType + ')'}</p>
-                  <p className="profile-details-explore-view mb-0">{artist.firstName + ' ' + artist.lastName}</p>
-                  <p className="profile-details-explore-view explore-view-city">{artist.city + ', ' + artist.country}</p>
-                  <Button className="btn btn-outline-dark btn-sm view-profile-button" onClick={handleViewProfile} name={artist.displayName}>View Profile</Button>
-                  <hr className="theme-break"/>
-                </div>
-              )
-            })}
-          </Jumbotron>
-        </Container>
       </div>
     )
   }
