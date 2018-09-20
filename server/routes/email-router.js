ef162ae10a9ef430bb997bf6ea9058fb49031e5a
FranzCSUF <fsalvador79@gmail.com>
2018-09-19 18:28:13 -0700

    Add a router that handles /email by sending the message via the Sendgrid API. #18

---
+++ server/routes/email-router.js
@@ -0,0 +1,16 @@
+require('dotenv/config')
+const { Router } = require('express')
+const sgMail = require('@sendgrid/mail')
+
+module.exports = function emailRouter() {
+
+  const router = new Router()
+
+  router.post('/', (req, res, next) => {
+    sgMail.setApiKey(process.env.SENDGRID_API_KEY)
+    const msg = req.body
+    sgMail.send(msg)
+      .catch(err => next(err))
+  })
+  return router
+}
