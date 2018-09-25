1203dd1d4a09ed9db9951d62b8ad17a64909814c
FranzCSUF <fsalvador79@gmail.com>
2018-09-25 15:25:05 -0700

    Add a property within the app state for search results. #31

--- client/components/search-bar.js
+++ client/components/search-bar.js
@@ -4,6 +4,9 @@ export default class SearchBar extends Component {
   constructor(props) {
     super(props)
     this.state = {
+      isLoading: false,
+      results: [],
+      value: ''
     }
   }
   render() {
