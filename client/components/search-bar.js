f83e9026da3bee401474df6e26de73f897b85e2c
FranzCSUF <fsalvador79@gmail.com>
2018-09-25 16:53:32 -0700

    Add a componentDidMount method that retrieves the artists collection from the volab-app database and stores the array of users in the dataBase property of the app state. #31

--- client/components/search-bar.js
+++ client/components/search-bar.js
@@ -5,11 +5,18 @@ export default class SearchBar extends Component {
     super(props)
     this.state = {
       isLoading: false,
-      results: [],
-      value: ''
+      dataBase: []
     }
   }
+  componentDidMount() {
+    fetch('/artists/')
+      .then(res => res.ok ? res.json() : null)
+      .then(artists => artists && this.setState({ dataBase: artists }))
+      .catch(err => console.error(err))
+  }
+
   render() {
+    console.log(this.state)
     return (
       <form className="form-inline my-2 my-lg-0">
         <input className="form-control form-control-sm mr-sm-3 search-box" type="search" placeholder="Search" aria-label="Search"/>
