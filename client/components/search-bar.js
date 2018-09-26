3bc20ebad33ccc1452afc44c2c78b168f3205dfa
FranzCSUF <fsalvador79@gmail.com>
2018-09-25 23:13:22 -0700

    add a handleSearch event handler that updates the results property of the app state to artist profiles which have values that match the users input. #31

--- client/components/search-bar.js
+++ client/components/search-bar.js
@@ -5,8 +5,10 @@ export default class SearchBar extends Component {
     super(props)
     this.state = {
       isLoading: false,
-      dataBase: []
+      dataBase: [],
+      results: []
     }
+    this.handleSearch = this.handleSearch.bind(this)
   }
   componentDidMount() {
     fetch('/artists/')
@@ -15,11 +17,25 @@ export default class SearchBar extends Component {
       .catch(err => console.error(err))
   }
 
+  handleSearch(event) {
+    console.log(event.target.value)
+    const { dataBase } = this.state
+    const results = dataBase.filter(artist => {
+      for (let key in artist) {
+        if ((artist[key]) === event.target.value) {
+          return true
+        }
+      }
+    })
+    this.setState({ results })
+  }
+
   render() {
     console.log(this.state)
+    const { handleSearch } = this
     return (
       <form className="form-inline my-2 my-lg-0">
-        <input className="form-control form-control-sm mr-sm-3 search-box" type="search" placeholder="Search" aria-label="Search"/>
+        <input className="form-control form-control-sm mr-sm-3 search-box" type="search" placeholder="Search" aria-label="Search" onChange={handleSearch}/>
       </form>
     )
   }
