3416f9503606c4df2efcc8698f2a5763eb509498
FranzCSUF <fsalvador79@gmail.com>
2018-09-26 14:54:40 -0700

    Add an onBlur event handler handleOnBlur that removes the artists in the searchResults property of the app state so the DOM elements showing the artist's results is removed. #31

--- client/components/search-bar.js
+++ client/components/search-bar.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import { ListGroup, Button } from 'reactstrap'
 
 export default class SearchBar extends Component {
   constructor(props) {
@@ -9,6 +10,8 @@ export default class SearchBar extends Component {
       results: []
     }
     this.handleSearch = this.handleSearch.bind(this)
+    this.handleBlur = this.handleBlur.bind(this)
+    this.handleClick = this.handleClick.bind(this)
   }
   componentDidMount() {
     fetch('/artists/')
@@ -16,9 +19,7 @@ export default class SearchBar extends Component {
       .then(artists => artists && this.setState({ dataBase: artists }))
       .catch(err => console.error(err))
   }
-
   handleSearch(event) {
-    console.log(event.target.value)
     const { dataBase } = this.state
     const results = dataBase.filter(artist => {
       for (let key in artist) {
@@ -29,14 +30,37 @@ export default class SearchBar extends Component {
     })
     this.setState({ results })
   }
-
+  handleBlur(event) {
+    event.target.value = ''
+    this.setState({ results: [] })
+  }
+  handleClick(event) {
+    const artist = event.target.name
+    const { navigate } = this.props
+    navigate({ path: '#' + artist })
+    location.reload()
+  }
   render() {
     console.log(this.state)
-    const { handleSearch } = this
+    const { handleSearch, handleBlur, handleClick } = this
+    const { results } = this.state
     return (
-      <form className="form-inline my-2 my-lg-0">
-        <input className="form-control form-control-sm mr-sm-3 search-box" type="search" placeholder="Search" aria-label="Search" onChange={handleSearch}/>
-      </form>
+      <div>
+        <form className="form-inline my-2 my-lg-0">
+          <input className="form-control form-control-sm mt-1 mr-sm-3" type="search" placeholder="Search" aria-label="Search" onChange={handleSearch}/>
+        </form>
+        <ListGroup className={results.length ? 'search-list-results border' : 'search-list-group mt-1'} onMouseLeave={handleBlur}>
+          {results.map((artist, index) => {
+            return (
+              <Button className="search-list-item btn btn-outline-dark btn-sm p-2" key={index} name={artist.displayName} onClick={handleClick}>
+                <div className="profile-image-nav rounded-circle" style={{backgroundImage: `url(${artist.imageUrl})`}} key={index}></div>
+                <div className="search-list-item-display-name float-left ml-5">{artist.displayName}</div>
+              </Button>
+            )
+          })}
+        </ListGroup>
+      </div>
+
     )
   }
 }
