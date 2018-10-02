98213999a1ee98261bcc6a6f2150ae193d1c29ca
FranzCSUF <fsalvador79@gmail.com>
2018-10-02 12:45:03 -0700

    Put your props on a new line when they pass 80 characters wide. (containers) #42

--- client/containers/discover-artists.js
+++ client/containers/discover-artists.js
@@ -24,15 +24,30 @@ export default class DiscoverArtists extends Component {
       <div>
         <Container className="container bg-white mt-5">
           <Jumbotron className="bg-white pt-4 mb-0">
-            <h5 className="mb-5 font-weight-bold">{ view === 'Music%20Producer' ? 'Discover Producers' : 'Discover Vocalists' }</h5>
+            <h5 className="mb-5 font-weight-bold">
+              { view === 'Music%20Producer'
+                ? 'Discover Producers'
+                : 'Discover Vocalists' }
+            </h5>
             {filteredArtists.map((artist, index) => {
               return (
                 <div key= { index }>
-                  <div className="profile-image-explore-view float-md-left rounded-circle" style={{ backgroundImage: `url(${artist.imageUrl})` }}></div>
-                  <p className="profile-display-name-explore-view mb-0">{artist.displayName + ' (' + artist.artistType + ')'}</p>
-                  <p className="profile-details-explore-view mb-0">{artist.firstName + ' ' + artist.lastName}</p>
-                  <p className="profile-details-explore-view explore-view-city">{artist.city + ', ' + artist.country}</p>
-                  <Button className="btn btn-outline-dark btn-sm view-profile-button" href={'/#' + artist.displayName}>View Profile</Button>
+                  <div
+                    className="profile-image-explore-view float-md-left rounded-circle"
+                    style={{ backgroundImage: `url(${artist.imageUrl})` }}></div>
+                  <p className="profile-display-name-explore-view mb-0">
+                    {artist.displayName + ' (' + artist.artistType + ')'}
+                  </p>
+                  <p className="profile-details-explore-view mb-0">
+                    {artist.firstName + ' ' + artist.lastName}
+                  </p>
+                  <p className="profile-details-explore-view explore-view-city">
+                    {artist.city + ', ' + artist.country}
+                  </p>
+                  <Button className="btn btn-outline-dark btn-sm view-profile-button"
+                    href={'/#' + artist.displayName}>
+                    View Profile
+                  </Button>
                   <hr className="theme-break"/>
                 </div>
               )
