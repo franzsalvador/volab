6d9d13388b5bb5570d0f339025164d922375f29f
FranzCSUF <fsalvador79@gmail.com>
2018-10-04 16:00:39 -0700

    Created separate search bar list component and replaced with react element SearchListItems. #43

--- client/components/search-bar.js
+++ client/components/search-bar.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import { ListGroup } from 'reactstrap'
+import SearchListItems from './search-list-items'
 
 export default class SearchBar extends Component {
   constructor(props) {
@@ -58,22 +59,9 @@ export default class SearchBar extends Component {
         <ListGroup
           className={listGroupClass}
           onMouseLeave={handleBlur}>
-          {results.map((artist, index) => {
-            return (
-              <a
-                className="search-list-item btn btn-outline-dark btn-sm p-2"
-                key={index} onClick={handleClick}
-                href={'#' + artist.displayName}>
-                <div
-                  className="profile-image-nav rounded-circle"
-                  style={{backgroundImage: `url(${artist.imageUrl})`}}></div>
-                <div
-                  className="search-list-item-display-name float-left ml-5">
-                  {artist.displayName}
-                </div>
-              </a>
-            )
-          })}
+          <SearchListItems
+            handleClick = {handleClick}
+            results = {results}/>
         </ListGroup>
       </div>
 
