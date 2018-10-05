e9a41cf721e72150f0c0d66d888f0b9512a79053
FranzCSUF <fsalvador79@gmail.com>
2018-10-05 15:04:14 -0700

    Make a dedicated JSON client object to do your fetching in utils. #43

---
+++ client/util/fetch.js
@@ -0,0 +1,34 @@
+export function sendFetch(url, req, updateFunction) {
+  return (
+    fetch(url, req)
+      .then(res => res.ok ? res.json() : null)
+      .then(artist => updateFunction(artist))
+      .catch(err => console.error(err))
+  )
+}
+
+export function get(url, updateFunction) {
+  return (
+    fetch(url)
+      .then(res => res.ok ? res.json() : null)
+      .then(found => updateFunction(found))
+      .catch(err => console.error(err))
+  )
+}
+
+export function post(url, req) {
+  return (
+    fetch(url, req)
+      .then(res => res.ok ? res.json() : null)
+      .catch(err => console.error(err))
+  )
+}
+
+export function remove(url, req, updateFunction) {
+  return (
+    fetch(url, req)
+      .then(res => res.ok)
+      .then(updateFunction)
+      .catch(err => console.error(err))
+  )
+}
